bb70263d6350be6b43da4ab12804072f5f5a567c
eh158 <eh158@duke.edu>
2018-10-25 19:56:41 -0400

    Added first test file

--- HeartRateMonitor/HRM.py
+++ HeartRateMonitor/HRM.py
@@ -1,6 +1,43 @@
+import numpy as np
+import peakutils
+
+
+def find_beats():
+    return 0
+
+
+def find_num_beats():
+    return 0
+
+
+def find_voltage_extremes():
+    return 0
+
+
+def find_mean_hr_bpm():
+    return 0
+
+
+def find_peaks(time,voltages):
+    cb = np.array(voltages)
+    indexes = peakutils.indexes(cb)
+    return indexes
+
+
+def process_file(filename):
+    csv_file = np.genfromtxt(my_file, delimiter=",")
+    times = []
+    voltages = []
+    for i in csv_file:
+        times.append(i[0])
+        voltages.append(i[1])
+    return [times, voltages]
 
 
 if __name__ == "__main__":
     print("HRM")
-
-    
+    # read in data from CSV file
+    my_file = "test1.csv"
+    data = process_file(my_file)
+    peak_indices = find_peaks(data[0],data[1])
+    print(peak_indices)
---
+++ HeartRateMonitor/test_find_peaks.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.parametrize("given,expected", [
+	([0 1 0], 1),
+	([-1 1 -1], 1),
+	([0 0 0], 0)
+])
+def test_find_peaks(given, expected):
+    from HRM import find_peaks
+    assert find_peaks(given) == expected
