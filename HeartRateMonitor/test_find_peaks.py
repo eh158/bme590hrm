efe2c4f5ccd8d9148e9da63397849cfa5863d01f
eh158 <eh158@duke.edu>
2018-10-26 19:39:26 -0400

    Attempted to fix pep8

--- HeartRateMonitor/test_find_peaks.py
+++ HeartRateMonitor/test_find_peaks.py
@@ -5,7 +5,7 @@ import pytest
     ([0, 1, 0], 1),
     ([-1, 1, -1], 1),
     ([0, 0, 1, 0], 2),
-    ([-2, 1. - 2. - 2, 0. - 2], [1, 4])
+    ([-2, 1, -2, -2, 0, -2], [1, 4])
 ])
 def test_find_peaks(given, expected):
     from HRM import find_peaks
