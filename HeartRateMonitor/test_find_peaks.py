05af6e8bceeac941637c1efb633f269ab8f9baf1
eh158 <eh158@duke.edu>
2018-10-26 10:35:29 -0400

    Changed test case

--- HeartRateMonitor/test_find_peaks.py
+++ HeartRateMonitor/test_find_peaks.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.parametrize("given,expected", [
     ([0, 1, 0], 1),
     ([-1, 1, -1], 1),
-    ([0, 0, 1], 2)
+    ([0, 0, 1, 0], 2)
 ])
 def test_find_peaks(given, expected):
     from HRM import find_peaks
