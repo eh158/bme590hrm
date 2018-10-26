3d3269747bfab2bb34f2bede7c76c1e367a0347d
eh158 <eh158@duke.edu>
2018-10-26 19:23:31 -0400

    Added more exception handling

--- HeartRateMonitor/HRM.py
+++ HeartRateMonitor/HRM.py
@@ -2,6 +2,8 @@ import numpy as np
 import peakutils
 import json
 import os.path
+import sys
+from warnings import warn
 
 
 def fill_metrics(metrics, data, interval):
@@ -24,7 +26,7 @@ def find_beats(metrics, data):
     times = []
     for i in indexes:
         times.append(data[0][i])
-    metrics['beats'] = len(times)
+    metrics['beats'] = times
 
 
 def find_num_beats(metrics, data):
@@ -59,18 +61,20 @@ def find_peaks(voltages):
 def process_file(filename):
     csv_file = np.genfromtxt(my_file, delimiter=",")
     # add checker for correct formatting, and raise exception otherwise
+    if csv_file.shape[1] > 2:
+        warn("Check if data is time and voltage columnwise")
+    if csv_file.shape[1] < 2:
+        sys.exit("Insufficient data provided; both time and voltage needed.")
     times = []
     voltages = []
     for i in csv_file:
         times.append(i[0])
-        voltages.append(i[1])
+        voltages.append((i[1]))
     return [times, voltages]
 
 
 def gather_inputs(my_file, interval):
     data = process_file(my_file)
-    if(interval < 0):
-        interval = 20
     metrics = {}
     input = []
     input.append(metrics)
@@ -79,33 +83,55 @@ def gather_inputs(my_file, interval):
     return input
 
 
-if __name__ == "__main__":
-    print("HRM")
-    # read in data from CSV file
-    while True:
-        try:
-            my_file = input('Please specify the filename.\n')
-            if os.path.isfile(my_file):
-                if ".csv" in my_file:
-                    break
-                raise IOError
-            else:
-                print('File not found')
-        except IOError:
-            print('Please specify a csv file.')
-    # read in user input for interval
-    while True:
-        try:
-            interval = input('Please specify minute interval')
-            if interval.isdigit():
-                if(float(interval) <= 0):
-                    print('Please specify a positive interval')
+def get_file(my_file = None):
+    if my_file == None:
+        while True:
+            try:
+                my_file = input('Please specify the filename.\n')
+                if os.path.isfile(my_file):
+                    if ".csv" in my_file:
+                        break
+                    raise IOError
                 else:
-                    break
-            else:
-                raise IOError
-        except IOError:
-            print('Please provide a number for the interval.')
+                    print('File not found')
+            except IOError:
+                print('Please specify a csv file.')
+    if ".csv" in my_file:
+        return my_file
+    else:
+        sys.exit('File not a csv file')
+
+
+def get_interval(interval = None):
+    default_interval = 20
+    if interval == None:
+        while True:
+            try:
+                interval = input('Please specify minute interval')
+                if interval.isdigit():
+                    if(float(interval) <= 0):
+                        print('Please specify a positive interval')
+                    else:
+                        break
+                else:
+                    raise IOError
+            except IOError:
+                print('Please provide a number for the interval.')
+    if interval.isdigit():
+        if (float(interval) <= 0):
+            warn('Interval is negative, default interval will be used instead')
+            return default_interval
+        return interval
+    else:
+        warn('Interval invalid, default interval will be used instead')
+        return default_interval
+
+
+if __name__ == "__main__":
+    # read in data from CSV file
+    my_file = get_file('test1.csv')
+    # read in user input for interval
+    interval = get_interval('200')
     u_input = gather_inputs(my_file, float(interval))
     metrics = fill_metrics(u_input[0], u_input[1], u_input[2])
     print(metrics)
---
+++ HeartRateMonitor/test_HRM.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+@pytest.mark.parametrize("given,expected", [
+    ([0, 1, 0], 1),
+    ([-1, 1, -1], 1),
+    ([0, 0, 1, 0], 2),
+	([-2, 1. -2. -2, 0. -2],[1,4])
+])
+def test_find_peaks(given, expected):
+    from HRM import find_peaks
+    assert find_peaks(given) == expected
