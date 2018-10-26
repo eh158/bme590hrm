9461f27ecc5906eac364e3306a9d056b066e55a0
eh158 <eh158@duke.edu>
2018-10-26 10:24:20 -0400

    Added functions and fixed test values

--- HeartRateMonitor/HRM.py
+++ HeartRateMonitor/HRM.py
@@ -3,22 +3,39 @@ import peakutils
 
 
 def find_beats(metrics, data):
-    return 0
+    indexes = find_peaks(data[1])
+    times = []
+    for i in indexes:
+        times.append(data[i])
+    metrics['beats']=len(times)
 
 
 def find_num_beats(metrics, data):
-    return 0
+    indexes = find_peaks(data[1])
+    metrics['num_beats']=len(indexes)
 
 
 def find_voltage_extremes(metrics, data):
-    return 0
+    min_v = min(data[1])
+    max_v = max(data[1])
+    metrics['voltage_extremes']=(min_v,max_v)
 
 
-def find_mean_hr_bpm(interval, metrics, data):
-    return 0
+def find_mean_hr_bpm(metrics, data ,*args):
+    if len(args)==0:
+        interval = data[0][len(data[0])]
+    else:
+        interval = args
+    indexes = find_peaks(data[1])
+    beats = 0
+    for i in indexes:
+        if data[0][i]<=interval:
+            beats += 1
+    mean_hr = float(beats)/float(interval)
+    metrics['mean_hr_bpm'] = mean_hr
 
 
-def find_peaks(time,voltages):
+def find_peaks(voltages):
     cb = np.array(voltages)
     indexes = peakutils.indexes(cb)
     return indexes
