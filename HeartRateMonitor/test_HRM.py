52b0315a3f4d72dd01d41c3b1b39a179f417c4e5
eh158 <eh158@duke.edu>
2018-10-26 19:34:14 -0400

    Attempted to fix pep8

--- HeartRateMonitor/test_HRM.py
+++ HeartRateMonitor/test_HRM.py
@@ -6,7 +6,7 @@ from HRM import *
     ([0, 1, 0], 1),
     ([-1, 1, -1], 1),
     ([0, 0, 1, 0], 2),
-	([-2, 1. -2. -2, 0. -2],[1,4])
+    ([-2, 1. - 2. - 2, 0. - 2], [1, 4])
 ])
 def test_find_peaks(given, expected):
     assert find_peaks(given) == expected
@@ -16,10 +16,10 @@ def test_find_peaks(given, expected):
     (20, 20),
     (-1, 20),
     (10, 10),
-	(2.3, 2.3),
+    (2.3, 2.3),
     ('2.3', 20),
     ('a', 20),
-    ('helloooooo',20)
+    ('helloooooo', 20)
 ])
 def test_get_interval(agiven, aexpected):
     assert get_interval(agiven) == aexpected
