c3e2c2341c9019959acec454e6ee63005e750d34
eh158 <eh158@duke.edu>
2018-10-26 20:08:20 -0400

    Fixed unit testing errors

--- HeartRateMonitor/test_HRM.py
+++ HeartRateMonitor/test_HRM.py
@@ -5,12 +5,11 @@ from HRM import *
 @pytest.mark.parametrize("given,expected", [
     ([0, 1, 0], 1),
     ([-1, 1, -1], 1),
-    ([0, 0, 1, 0], 2),
-    ([-2, 1, -2, -2, 0, -2], [1, 4])
+    ([0, 0, 1, 0], 2)
 ])
 def test_find_peaks(given, expected):
     assert find_peaks(given) == expected
-
+    assert find_peaks([-2, 1, -2, -2, 0, -2]) == [1, 4]
 
 @pytest.mark.parametrize("agiven,aexpected", [
     (20, 20),
