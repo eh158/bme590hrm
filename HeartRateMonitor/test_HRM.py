465a3992c2281fb05594a6287b246bffe20f1b09
eh158 <eh158@duke.edu>
2018-10-28 03:42:36 -0400

    Fixed formatting

--- HeartRateMonitor/test_HRM.py
+++ HeartRateMonitor/test_HRM.py
@@ -81,7 +81,7 @@ def test_process_output(metrics, filename, f2, jn, expected, detected):
         filewriter = csv.writer(csvfile, delimiter=',', quotechar='|',
                                 quoting=csv.QUOTE_MINIMAL)
         for i in range(5):
-            filewriter.writerow([i, i%2])
+            filewriter.writerow([i, i % 2])
     try:
         process_output(metrics, filename)
     except ValueError:
@@ -143,4 +143,3 @@ def test_find_num_beats(metrics, data, expected):
 ])
 def test_fill_metrics(metrics, data, interval, expected):
     assert fill_metrics(metrics, data, interval) == expected
-
