7a4aebc5e8f13e7b686d723edf7ab64f62709264
eh158 <eh158@duke.edu>
2018-10-28 00:39:22 -0400

    Added fill_metrics test case

--- HeartRateMonitor/test_HRM.py
+++ HeartRateMonitor/test_HRM.py
@@ -114,15 +114,15 @@ def test_find_mean_hr_bpm(metrics, data, interval, expected):
 def test_find_voltage_extremes(metrics, data, expected):
     assert find_voltage_extremes(metrics, data) == expected
 
+
 @pytest.mark.parametrize("metrics, data, expected", [
     ({}, [[0, 1, 2, 3, 4, 5], [1, 2, 1, 2, 1, 1]], {'num_beats': 2})
 ])
 def test_find_num_beats(metrics, data, expected):
     assert find_num_beats(metrics, data) == expected
 
-
-# @pytest.mark.parametrize("metrics, data, expected", [
-#     ({}, [[0, 1, 2, 3, 4, 5], [1, 2, 1, 2, 1, 1]], {'beats': [1, 3]})
-# ])
-# def test_fill_metrics(metrics, data, expected):
-#     assert fill_metrics(metrics, data) == expected
+@pytest.mark.parametrize("metrics, data, expected", [
+    ({}, [[0, 1, 2, 3, 4, 5], [1, 2, 1, 2, 1, 1]], {"duration": 0.06666666666666667, "beats": [1.0, 3.0], "num_beats": 2, "voltage_extremes": [1.0, 2.0], "mean_hr_bpm": 30.0})
+])
+def test_fill_metrics(metrics, data, expected):
+    assert fill_metrics(metrics, data) == expected
