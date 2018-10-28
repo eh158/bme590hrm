69732b2f15d92d19f9bf088f913b79f0b16dae0a
eh158 <eh158@duke.edu>
2018-10-27 22:21:08 -0400

    Added process output test case

--- HeartRateMonitor/test_HRM.py
+++ HeartRateMonitor/test_HRM.py
@@ -69,11 +69,11 @@ def test_find_duration(metrics, data, expected):
 def test_find_beats(metrics, data, expected):
     assert find_beats(metrics, data) == expected
 
-# @pytest.mark.parametrize("metrics, data, expected", [
-#     ({}, [[0, 1, 2, 3, 4, 5], [1, 2, 1, 2, 1, 1]], {'beats': [1, 3]})
-# ])
-# def test_process_output(metrics, filename, expected):
-#     assert process_output(metrics, filename) == expected
+@pytest.mark.parametrize("metrics, filename, expected", [
+    ({}, 'test.csv', json.dump({},'test.json'))
+])
+def test_process_output(metrics, filename, expected):
+    assert process_output(metrics, filename) == expected
 #
 #
 # @pytest.mark.parametrize("metrics, data, expected", [
