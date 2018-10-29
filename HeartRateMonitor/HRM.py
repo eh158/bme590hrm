fe71fdc944a88f5308e7cbcf1876d2c0c031f5c9
eh158 <eh158@duke.edu>
2018-10-28 20:53:04 -0400

    exceptions testing

--- HeartRateMonitor/HRM.py
+++ HeartRateMonitor/HRM.py
@@ -60,7 +60,7 @@ def find_voltage_extremes(metrics, data):
 
 def find_mean_hr_bpm(metrics, data, time_interval):
     x = [i for i in data[0] if isinstance(i, str)]
-    x = [i for i in data[1] if isinstance(i, str)]
+    y = [i for i in data[1] if isinstance(i, str)]
     if len(x) > 0 or len(y) > 0:
         raise ValueError('String not expected')
     if time_interval > data[0][len(data[0]) - 1]:
